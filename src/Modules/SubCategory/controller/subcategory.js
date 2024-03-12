37f85f668c6fe44b43663d6823313a64346b4d26
salah <salahsaamir707@gmail.com>
2024-03-12 16:38:04 +0200

    update SubCategory

--- src/Modules/SubCategory/controller/subcategory.js
+++ src/Modules/SubCategory/controller/subcategory.js
@@ -32,3 +32,14 @@ export const GetSpecialSubCategory=AsyncHandeller(
         return subcategory? res.status(200).json({message:"done",subcategory}):next(new Error("subcategory not found"))
     }
 )
+export const UpdateSubCategory=AsyncHandeller(
+    async(req,res,next)=>{
+        const {id}=req.params
+        const {name,image}=req.body
+        if(await SubCategoryCollection.findOne({name})){
+            return next(new Error("subcategory already exist",{cause:400}))
+        }
+        const subcategory=await SubCategoryCollection.findByIdAndUpdate(id,req.body,{new:true})
+        return subcategory? res.status(200).json({message:"done",subcategory}):next(new Error("subcategory not updated"))
+    }
+)
